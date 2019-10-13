19f32a49abb0e1cc79eac741ce51de88db6be7f9
fivimelinda <54924175+fivimelinda@users.noreply.github.com>
2019-10-13 22:12:17 +0700

    Inisiasi (#1)
    * add: models
    
    * fixing models

---
+++ sibat/src/main/java/tugas1/sibat/model/GudangModel.java
@@ -0,0 +1,66 @@
+package tugas1.sibat.model;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name="gudang")
+public class GudangModel implements Serializable {
+	@Id
+	@Size(max = 20)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idGudang;
+	
+	@NotNull
+	@Size(max = 255)
+	@Column(name="nama_gudang", nullable = false)
+	private String namaGudang;
+	
+	@NotNull
+	@Size(max = 255)
+	@Column(name="alamat_gudang", nullable = false)
+	private String alamatGudang;
+	
+	@OneToMany(mappedBy = "gudang", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<GudangObatModel> listGudangObat;
+
+	public Long getIdGudang() {
+		return idGudang;
+	}
+
+	public void setIdGudang(Long idGudang) {
+		this.idGudang = idGudang;
+	}
+
+	public String getNamaGudang() {
+		return namaGudang;
+	}
+
+	public void setNamaGudang(String namaGudang) {
+		this.namaGudang = namaGudang;
+	}
+
+	public String getAlamatGudang() {
+		return alamatGudang;
+	}
+
+	public void setAlamatGudang(String alamatGudang) {
+		this.alamatGudang = alamatGudang;
+	}
+
+
+}
---
+++ sibat/src/main/java/tugas1/sibat/model/GudangObatModel.java
@@ -0,0 +1,69 @@
+package tugas1.sibat.model;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="gudang_obat")
+public class GudangObatModel implements Serializable{
+	@Id
+	@Size(max = 20)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idGudangObat;
+	
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
+	@JoinColumn(name = "id_obat", referencedColumnName= "idObat", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	private ObatModel obat;
+	
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
+	@JoinColumn(name = "id_gudang", referencedColumnName= "idGudang", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	private GudangModel gudang;
+
+	public Long getIdGudangObat() {
+		return idGudangObat;
+	}
+
+	public void setIdGudangObat(Long idGudangObat) {
+		this.idGudangObat = idGudangObat;
+	}
+
+	public ObatModel getObat() {
+		return obat;
+	}
+
+	public void setObat(ObatModel obat) {
+		this.obat = obat;
+	}
+
+	public GudangModel getGudang() {
+		return gudang;
+	}
+
+	public void setGudang(GudangModel gudang) {
+		this.gudang = gudang;
+	}
+	
+
+}
