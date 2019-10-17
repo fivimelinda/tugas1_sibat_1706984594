6a76ef1f6bdd929211e456c54a3c354f7e84b67c
Fivi Melinda <fivimelinda@gmail.com>
2019-10-17 19:58:05 +0700

    add: viewall-obat.html

--- sibat/src/main/java/tugas1/sibat/controller/ObatController.java
+++ sibat/src/main/java/tugas1/sibat/controller/ObatController.java
@@ -1,5 +1,37 @@
 package tugas1.sibat.controller;
 
-public class ObatController {
+import java.util.Collections;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import tugas1.sibat.model.ObatModel;
+import tugas1.sibat.service.ObatService;
+
+
+@Controller
+public class ObatController {
+	
+	@Qualifier("obatServiceImpl")
+	@Autowired
+	private ObatService obatService;
+	
+	@RequestMapping("/")
+	public String home() { return "home";}
+	
+	//URL mapping view
+		@RequestMapping("/obat/view-all")
+		public String viewall(Model model) {
+			
+			//Mengambil semua objek ObatModel yang ada
+			List<ObatModel> listObat = obatService.getObatList();
+			//Add model restoran ke "resto" untuk dirender
+			model.addAttribute("obatList", listObat);
+			//return view template
+			return "viewall-obat";
+		}
 }
