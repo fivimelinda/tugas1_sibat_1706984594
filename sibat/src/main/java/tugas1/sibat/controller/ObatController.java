f23c6c56d17ba8e359c21079745cac57c499252d
Fivi Melinda <fivimelinda@gmail.com>
2019-10-18 18:15:52 +0700

    fix: feat detail data obat

--- sibat/src/main/java/tugas1/sibat/controller/ObatController.java
+++ sibat/src/main/java/tugas1/sibat/controller/ObatController.java
@@ -11,21 +11,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import tugas1.sibat.model.JenisModel;
 import tugas1.sibat.model.ObatModel;
-import tugas1.sibat.service.JenisService;
 import tugas1.sibat.service.ObatService;
-
 
 @Controller
 public class ObatController {
 	
-	@Qualifier("obatServiceImpl")
+//	@Qualifier("obatServiceImpl")
 	@Autowired
 	private ObatService obatService;
-	
-	@Autowired
-	private JenisService jenisService;
 	
 	//URL mapping view all obat
 	@RequestMapping("/")
@@ -43,8 +37,6 @@ public class ObatController {
 	@RequestMapping(value ="/obat/view/{nomorRegistrasi}", method = RequestMethod.GET)
 	public String viewByNoReg(@PathVariable String nomorRegistrasi, Model model) {
 		ObatModel existingObat = obatService.getObatByNomorRegistrasi(nomorRegistrasi);
-		JenisModel jenisObat = jenisService.getJenisByIdObat(existingObat.getIdObat());
-		existingObat.setJenis(jenisObat);
 		model.addAttribute("obat", existingObat);
 		return "view-obat";
 	}
